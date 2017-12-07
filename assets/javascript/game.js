a4f8a27b1189a907ea1fc2cb520d7404d1291013
Li Chien <lichien@Lis-MacBook-Pro.local>
2017-12-07 18:14:46 -0500

    its DONE

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,80 @@
-
+window.onload = function() {
 
 // create arrays
 let letterBank = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-let randNum = getRandomNumber(26);
-let computerGuess = letterBank[randNum];
-let wrongLetter = [];
-let winCounter = 0;
-let lossCounter = 0;
-let guessesLeftCounter = 10;
+let wrongLetter = []; //array of users previous guesses
+let winCounter = 0; 
+let lossCounter = 0; 
+let guessesLeftCounter = 9; 
+let letterToGuess = null;
 // define variables userGuess, computerGuess
 
-function getRandomNumber(upper) {
-	var num = Math.floor(Math.random()*upper) + 1;
-	return num;
-}
-;
+//computer picks a random number and assigns it to an index number in the letterBank, thus chosing a letter for the user to guess
+//tl;dr computers guess
+let computerGuess = letterBank[Math.floor(Math.random() * letterBank.length)];
 
 console.log(computerGuess);
 
-function roundReset() {
-	let wrongLetter = [];
-	let guessesLeftCounter = 10;
-	return computerGuess;
+//k so user starts off with 9 guesses
+//function that changes guessesLeftCounter displayed on document
+function updateGuessesLeftCounter() {
+  // writes the number of guesses we have left in the document
+  document.getElementById('guessesLeft').innerHTML = guessesLeftCounter;
+};
+
+//makes the computer choose another random letter to guess
+function updateLetterToGuess() {
+  letterToGuess = letterBank[Math.floor(Math.random() * letterBank.length)];
+};
+
+//function that changes wrongLetter array displayed in document
+function updateWrongLetterArray() {
+	document.getElementById('wrong').innerHTML = wrongLetter.join(' ');
 }
 
 
-// event function about pressing down a key
-document.addEventListener('keypress', (event) => {
-	let userGuess = String.fromCharCode(event.keyCode);
+//reset errythang
+let roundReset = function() {
+	wrongLetter = [];
+	guessesLeftCounter = 9;
+	computerGuess = letterBank[Math.floor(Math.random() * letterBank.length)];
+	console.log(computerGuess);
 
-	if (userGuess == computerGuess) {
-		document.getElementById('wins').innerHTML = winCounter++;
-		roundReset();
-	} else if (guessesLeftCounter == 0) {
-			document.getElementById('losses').innerHTML = lossCounter++;
-			document.getElementById('guessesLeft').innerHTML = guessesLeftCounter;
-			roundReset();
-	} else {
-		wrongLetter.push(userGuess);
-		document.getElementById('wrong').innerHTML = wrongLetter.join(' ');
-		console.log(wrongLetter);
-		document.getElementById('guessesLeft').innerHTML = guessesLeftCounter--;
-	}
+	updateGuessesLeftCounter();
+	updateLetterToGuess();
 
-	
+}
+
+	updateGuessesLeftCounter();
+	updateLetterToGuess();
+
+
+//When key is pressed it becomes the users guess
+document.onkeyup = function(event) {
+  	let userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+  	
+  	guessesLeftCounter = guessesLeftCounter - 1;
+
+    if (guessesLeftCounter > 0) {
+        if (userGuess == computerGuess) {
+            	winCounter = winCounter + 1;
+            	document.querySelector('#wins').innerHTML = winCounter;
+            	roundReset();
+            } else {
+                guessesLeft = guessesLeft - 1;
+                updateGuessesLeftCounter();
+            	wrongLetter.push(userGuess);
+            	updateWrongLetterArray()
+            }
+    } else if (guessesLeftCounter == 0) {
+        lossCounter = lossCounter + 1;
+        document.querySelector('#losses').innerHTML = lossCounter;
+        roundReset();
+    }
+};
+
+
+
 		
 
-		
-	});
-
-
-// if userGuess === computerGuess, then add 1 to winCounter var
-
-
-// else userGuess !== computerGuess, then add userGuess to wrongLetter array
-
-// if var guessesLeftCounter = 0, then alert you lose sucker && add 1 to lossCounter var
-
-
-
-
-
-
+}
